c40331398b357c10823a9b6153b5efaddb0366dd
DanielNikoM <daniel.niko@ui.ac.id>
2023-11-17 20:42:06 +0700

    CS08 - Menyelesaikan CS modul 8

--- app/src/main/java/com/DanielNikoMJbusIO/jbus_android/LoginActivity.java
+++ app/src/main/java/com/DanielNikoMJbusIO/jbus_android/LoginActivity.java
@@ -2,13 +2,40 @@ package com.DanielNikoMJbusIO.jbus_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView registerNow = null;
+    private Button loginButton = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registerNow = findViewById(R.id.RegisterNow);
+        loginButton = findViewById(R.id.Login);
+
+        registerNow.setOnClickListener(v -> {moveActivity(this, RegisterActivity.class);});
+        loginButton.setOnClickListener(v -> {moveActivity(this, MainActivity.class);});
+
+        getSupportActionBar().hide();
     }
+    private void moveActivity(Context ctx, Class<?> cls){
+        Intent intent = new Intent(ctx, cls);
+        startActivity(intent);
+    }
+
+    private void viewToast(Context ctx, String message){
+        Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
+    }
+
+
+
+
 }
