609e6dea9d4e48d6988f55ce1fae926f3cde3ed2
DanielNikoM <daniel.niko@ui.ac.id>
2023-11-23 23:49:48 +0700

    TP09 - Menyelesaikan TP modul 9

---
+++ app/src/main/java/com/DanielNikoMJbusIO/jbus_android/request/BaseApiService.java
@@ -0,0 +1,13 @@
+package com.DanielNikoMJbusIO.jbus_android.request;
+import com.DanielNikoMJbusIO.jbus_android.model.Account;
+import com.DanielNikoMJbusIO.jbus_android.model.BaseResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface BaseApiService {
+    @GET("account/{id}")
+    Call<Account> getAccountbyId (@Path("id") int id);
+}
